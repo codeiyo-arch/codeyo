350181e32f079061171a628af80239096f7ab447
codeiyo-arch <codeiyo001@gmail.com>
2026-04-08 17:10:29 +0500

    Delete src directory

--- src/components/RoadmapWrapper.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import dynamic from "next/dynamic";
-
-const Roadmap = dynamic(() => import("@/components/Roadmap"), { ssr: false });
-
-export default function RoadmapWrapper() {
-  return <Roadmap />;
-}
